229e7f86496e18db3cdb43905477f8fb9257ef56
cc <17740852054@163.com>
2018-04-24 02:55:38 +0800

    first add

--- taotaomanage/taotao-manage-web/src/main/java/com/taotao/manage/controller/PicUploadController.java
+++ taotaomanage/taotao-manage-web/src/main/java/com/taotao/manage/controller/PicUploadController.java
@@ -5,11 +5,14 @@ import com.taotao.manage.service.PropertiesService;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -22,8 +25,9 @@ import java.util.List;
 
 @Controller
 @RequestMapping("pic/upload")
-public class PicUpLoadController {
-    private static final List<String> extentions = Arrays.asList("jpg", "jpeg", "gif", "png", "bmp");
+public class PicUploadController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PicUploadController.class);
+    private static final List<String> extentions = Arrays.asList("jpg", "jpeg", "png", "bmp", "gif");
     @Autowired
     private PropertiesService propretiesService;
 
@@ -36,6 +40,7 @@ public class PicUpLoadController {
      * @return
      */
     @PostMapping
+    @ResponseBody
     public PicUploadResult uploadPicture(@RequestParam("uploadFile") MultipartFile uploadFile) {
         //设置图片上传结果
         PicUploadResult picUploadResult = new PicUploadResult();
@@ -49,7 +54,7 @@ public class PicUpLoadController {
         try {
             //获取文件名, 验证后缀
             String originalFilename = uploadFile.getOriginalFilename();
-            if (!extentions.contains(StringUtils.substringBeforeLast(originalFilename, "."))) {
+            if (!extentions.contains(StringUtils.substringAfterLast(originalFilename, "."))) {
                 picUploadResult.setMessage("图片格式不正确!");
                 return picUploadResult;
             }
@@ -64,6 +69,10 @@ public class PicUpLoadController {
             picUploadResult.setError(0).setMessage("图片上传成功");
             String filePath = getFilePath(originalFilename);
             uploadFile.transferTo(new File(filePath));
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Pic file upload .[{}] to [{}] .", uploadFile.getOriginalFilename(), filePath);
+            }
+
             String url = StringUtils.replace(StringUtils.replace(filePath, propretiesService.REPOSITORY_PATH, propretiesService.IMAGE_URL), "\\", "/");
             picUploadResult.setUrl(url).setMessage(null);
         } catch (IOException e) {
@@ -74,7 +83,7 @@ public class PicUpLoadController {
 
     }
 
-    @org.jetbrains.annotations.NotNull
+
     private String getFilePath(String sourceFileName) {
         String baseFolder = this.propretiesService.REPOSITORY_PATH + File.separator + "images";
         Date nowDate = new Date();
