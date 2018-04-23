bd0be8e4d391fda405481f666c6d54d7c6726e83
wanglong1992 <962549004@qq.com>
2018-04-24 00:07:25 +0800

    first add

--- taotaomanage/taotao-manage-web/src/main/java/com/taotao/manage/controller/PicUploadController.java
+++ taotaomanage/taotao-manage-web/src/main/java/com/taotao/manage/controller/PicUploadController.java
@@ -1,125 +1,95 @@
-/*
 package com.taotao.manage.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.taotao.manage.pojo.PicUploadResult;
+import com.taotao.common.pojo.PicUploadResult;
+import com.taotao.manage.service.PropertiesService;
 import org.apache.commons.lang3.RandomUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
-
-///import com.taotao.store.manage.controller.result.PicUploadResult;
-//import com.taotao.store.manage.service.PropertieService;
-//import org.joda.time.DateTime;
-
-*/
-/**
- * 图片上传
- *//*
+import java.util.List;
 
 @Controller
-@RequestMapping("/pic")
-public class PicUploadController {
+@RequestMapping("pic/upload")
+public class PicUpLoadController {
+    private static final List<String> extentions = Arrays.asList("jpg", "jpeg", "gif", "png", "bmp");
+    @Autowired
+    private PropertiesService propretiesService;
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(PicUploadController.class);
+    /**
+     * 1.大小
+     * 2.是不是图片：后缀
+     * 3.文件内容
+     *
+     * @param uploadFile
+     * @return
+     */
+    @PostMapping
+    public PicUploadResult uploadPicture(@RequestParam("uploadFile") MultipartFile uploadFile) {
+        //设置图片上传结果
+        PicUploadResult picUploadResult = new PicUploadResult();
+        //默认不成功
+        picUploadResult.setError(1).setMessage("文件上传不成功");
+        //没上传文件
+        if (uploadFile == null) {
+            return picUploadResult;
+        }
 
-	@Autowired
-	private PropertieService propertieService;
-	
-	private static final ObjectMapper mapper = new ObjectMapper();
+        try {
+            //获取文件名, 验证后缀
+            String originalFilename = uploadFile.getOriginalFilename();
+            if (!extentions.contains(StringUtils.substringBeforeLast(originalFilename, "."))) {
+                picUploadResult.setMessage("图片格式不正确!");
+                return picUploadResult;
+            }
+            // 验证内容
+            // 验证码
+            BufferedImage image = ImageIO.read(uploadFile.getInputStream());
+            if (image == null) {
+                picUploadResult.setMessage(null);
+                return picUploadResult;
+            }
+            // 验证通过
+            picUploadResult.setError(0).setMessage("图片上传成功");
+            String filePath = getFilePath(originalFilename);
+            uploadFile.transferTo(new File(filePath));
+            String url = StringUtils.replace(StringUtils.replace(filePath, propretiesService.REPOSITORY_PATH, propretiesService.IMAGE_URL), "\\", "/");
+            picUploadResult.setUrl(url).setMessage(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return picUploadResult;
 
-	// 允许上传的格式
-	private static final String[] IMAGE_TYPE = new String[] { ".bmp", ".jpg", ".jpeg", ".gif", ".png" };
 
-	@RequestMapping(value = "/upload", method = RequestMethod.POST)
-	@ResponseBody
-	public String upload(@RequestParam("uploadFile") MultipartFile uploadFile , HttpServletResponse response) throws Exception {
+    }
 
-		// 校验图片格式
-		boolean isLegal = false;
-		for (String type : IMAGE_TYPE) {
-			if (StringUtils.endsWithIgnoreCase(uploadFile.getOriginalFilename(), type)) {
-				isLegal = true;
-				break;
-			}
-		}
-
-		// 封装Result对象，并且将文件的byte数组放置到result对象中
-		PicUploadResult fileUploadResult = new PicUploadResult();
-
-		// 状态
-		fileUploadResult.setError(isLegal ? 0 : 1);
-
-		// 文件新路径
-		String filePath = getFilePath(uploadFile.getOriginalFilename());
-
-		if (LOGGER.isDebugEnabled()) {
-			LOGGER.debug("Pic file upload .[{}] to [{}] .", uploadFile.getOriginalFilename(), filePath);
-		}
-
-		// 生成图片的绝对引用地址
-		String picUrl = StringUtils.replace(StringUtils.substringAfter(filePath, propertieService.REPOSITORY_PATH), "\\", "/");
-		fileUploadResult.setUrl(propertieService.IMAGE_BASE_URL + picUrl);
-
-		File newFile = new File(filePath);
-
-		// 写文件到磁盘
-		uploadFile.transferTo(newFile);
-
-		// 校验图片是否合法
-		isLegal = false;
-		try {
-			BufferedImage image = ImageIO.read(newFile);
-			if (image != null) {
-				fileUploadResult.setWidth(image.getWidth() + "");
-				fileUploadResult.setHeight(image.getHeight() + "");
-				isLegal = true;
-			}
-		} catch (IOException e) {
-		}
-
-		// 状态
-		fileUploadResult.setError(isLegal ? 0 : 1);
-
-		if (!isLegal) {
-			// 不合法，将磁盘上的文件删除
-			newFile.delete();
-		}
-
-		response.setContentType(MediaType.TEXT_HTML_VALUE);
-		return mapper.writeValueAsString(fileUploadResult);
-	}
-
-	private String getFilePath(String sourceFileName) {
-		String baseFolder = propertieService.REPOSITORY_PATH + File.separator + "images";
-		Date nowDate = new Date();
-		// yyyy/MM/dd
-		String fileFolder = baseFolder + File.separator + new DateTime(nowDate).toString("yyyy") + File.separator + new DateTime(nowDate).toString("MM") + File.separator
-				+ new DateTime(nowDate).toString("dd");
-		File file = new File(fileFolder);
-		if (!file.isDirectory()) {
-			// 如果目录不存在，则创建目录
-			file.mkdirs();
-		}
-		// 生成新的文件名
-		String fileName = new DateTime(nowDate).toString("yyyyMMddhhmmssSSSS") + RandomUtils.nextInt(100, 9999) + "." + StringUtils.substringAfterLast(sourceFileName, ".");
-		return fileFolder + File.separator + fileName;
-	}
+    @org.jetbrains.annotations.NotNull
+    private String getFilePath(String sourceFileName) {
+        String baseFolder = this.propretiesService.REPOSITORY_PATH + File.separator + "images";
+        Date nowDate = new Date();
+        // yyyy/MM/dd
+        String fileFolder = baseFolder + File.separator + new DateTime(nowDate).toString("yyyy") + File.separator
+                + new DateTime(nowDate).toString("MM") + File.separator + new DateTime(nowDate).toString("dd");
+        File file = new File(fileFolder);
+        if (!file.isDirectory()) {
+            // 如果目录不存在，则创建目录
+            file.mkdirs();
+        }
+        // 生成新的文件名
+        String fileName = new DateTime(nowDate).toString("yyyyMMddhhmmssSSSS") + RandomUtils.nextInt(100, 9999) + "."
+                + StringUtils.substringAfterLast(sourceFileName, ".");
+        return fileFolder + File.separator + fileName;
+    }
 
 }
-*/
